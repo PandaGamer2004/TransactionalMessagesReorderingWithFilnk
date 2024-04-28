88cb565209fa1e22e9e46e8d9fd763b0cadc3731
Daniil Bazhanov <in8ey8eg2xj9w34ru4559akxot3hum@bots.bitbucket.org>
2024-04-28 23:32:16 +0200

    Staged flink kafka connector addition

--- message-middleware/message-middleware/src/main/java/org/daniil/MessageReorderingAndDeduplicationJob.java
+++ message-middleware/message-middleware/src/main/java/org/daniil/MessageReorderingAndDeduplicationJob.java
@@ -1,10 +1,15 @@
 package org.daniil;
 
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class MessageReorderingAndDeduplicationJob {
 
 	public static void main(String[] args) throws Exception {
+
+		KafkaSource<String> source = KafkaSource.<String>builder()
+				.setBootstrapServers("localhost:9092")
+				.setTopics("")
 		final StreamExecutionEnvironment env
 				= StreamExecutionEnvironment.getExecutionEnvironment();
 
